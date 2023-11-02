2830a7137d31bb2348e27381435e3a4b7ab58562
wangy-zj <ywang@zhejianglab.com>
2023-11-02 16:35:05 +0800

    '更新脚本，简化调用'

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -2,59 +2,82 @@
 
 echo="echo -e"
 
+trap ctrl_c INT
+
+function ctrl_c() {
+    $echo "** Trapped CTRL-C"
+    cleanup
+    exit
+}
+
+declare -a pids
+declare -a keys
+
+function cleanup {
+    if [ ${#pids[@]} -gt 0 ]
+    then
+	$echo "existing pids are ${pids[@]}"
+	for pid in "${pids[@]}"
+	do
+	    $echo "killing pid $pid"
+	    kill -9 $pid
+	done
+    else
+	$echo "we do not have any existing processes"
+    fi
+    
+    if [ ${#keys[@]} -gt 0 ]
+    then
+	$echo "existing ring buffer keys are ${keys[@]}"
+       for key in "${keys[@]}"
+       do
+    	   $echo "removing ring buffer $key"
+    	   dada_db -k $key -d
+       done
+    else
+	$echo "we do not have any existing ring buffers"
+    fi
+}
+
 $echo "This is a pipeline for beamforming\n"
 
-# There will be three ring buffers
+# There will be two ring buffers
 # The first one is used to receive raw data
 # The second one is used to receive beamformed data
-# The last one is used to receive zoom fft data
 
-# The first ring buffer need two readers as we need to read its data to GPU and write its data to disk
+# The first ring buffer need two readers if we also want to save the raw data
 # The second ring buffer has only one reader
-# The last ring buffer has only one reader as well
 
-WORK_ROOT=/home/hero/code
-project_root=$WORK_ROOT/PAF_pipeline
+# some path of the code
+project_root=/home/hero/code/PAF_pipeline
 hdr_root=$project_root/header
 beamform_command=$project_root/build/pipeline/pipeline_dada_beamform
 udp_command=$project_root/build/udp/udp2db
 
 $echo "Setting up ring buffers"
-
 key_raw=a000
 key_bmf=b000
-
-$echo "key_raw is $key_raw"
-$echo "key_bmf is $key_bmf"
-
 # it will be more flexible if we put equation here to calculate buffer size with some basic configrations
 bufsz_raw=25165824 # buffer block size to hold raw data in bytes, change it to real value later
 bufsz_bmf=20971 # buffer block size to hold beamformed data in bytes, change it to real value later
 
-$echo "bufsz_raw is $bufsz_raw"
-$echo "bufsz_bmf is $bufsz_bmf"
+$echo "key_raw is $key_raw, and bufsz_raw is $bufsz_raw"
+$echo "key_bmf is $key_bmf, and bufsz_bmf is $bufsz_bmf\n"
 
 numa=0 # numa node to use 
-$echo "numa is $numa\n"
 
 $echo "Creating ring buffers"
 dada_raw="dada_db -k $key_raw -b $bufsz_raw -p -w -c $numa -r 2"  #assign memory from NUMA node  [default: all nodes]
 dada_bmf="dada_db -k $key_bmf -b $bufsz_bmf -p -w -c $numa"
 
-$echo "dada_raw is $dada_raw"
-$echo "dada_bmf is $dada_bmf \n"
-
 $dada_raw & # should be unblock 
 $dada_bmf & # should be unblock
 
 sleep 1s # to wait all buffers are created 
-$echo "Setting file writers"
+$echo "\nSetting file writers"
 # different type of files should go to different directories
 dir_raw=/home/hero/data/data_raw/ # change it to real value later
 dir_bmf=/home/hero/data/data_bmf/ # change it to real value later
-
-$echo "dir_raw $dir_raw"
-$echo "dir_bmf $dir_bmf"
 
 # 设定写入数据的地址和数据来源ringbuffer
 writer_raw="dada_dbdisk -D $dir_raw -k $key_raw -W" 
@@ -66,39 +89,22 @@ $echo "writer_bmf $writer_bmf\n"
 $writer_raw & # should be unblock 
 $writer_bmf & # should be unblock
 
-# now gpu pipeline
-$echo "Starting process"
+# start beamform pipeline
+$echo "Start beamform process"
 process="$beamform_command -i $key_raw -o $key_bmf -g 0" # need to add other configurations as well
-$echo "process $process\n"
+$echo "$process\n"
 $process & # should be unblock
 
-# now udp2db
+# set up udp2db
 nblock=10
 nsecond=10
-freq=1420
 hdr_fname=$hdr_root/paf_test.header
 
-$echo "nblock is:    $nblock"
-$echo "nsecond is:   $nsecond"
-$echo "freq is:      $freq\n"
-
 # Start udp2db
-$udp_command -f $hdr_fname -k $key_raw -n $nblock -N $nsecond
+$echo "Start udp2db process"
+udp_process="$udp_command -f $hdr_fname -k $key_raw -n $nblock -N $nsecond"
+$echo "$udp_process\n"
+$udp_process
 sleep 1s
 $echo "done udp2db setup\n"
-
-sleep 1s # to wait all process finishes
-$echo "Killing dada_dbdisk"
-writer_d="pkill -9 dada_dbdisk" #
-$echo "writer_d $writer_d\n"
-$writer_d # we should not block here 
-
-$echo "\nRemoving ring buffers"
-dada_raw_d="dada_db -k $key_raw -d"
-dada_bmf_d="dada_db -k $key_bmf -d"
-
-$echo "dada_raw_d is $dada_raw_d"
-$echo "dada_bmf_d is $dada_bmf_d\n"
-
-$dada_raw_d
-$dada_bmf_d
+cleanup
