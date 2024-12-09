fa2de8f185285bbdf88cbfb511ad2913488f6690
wangy-zj <ywang@zhejianglab.com>
2024-12-09 11:19:15 +0800

    'add dada_speed_test.sh'

---
+++ scripts/dada_speed_test.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+
+echo="echo -e"
+
+trap ctrl_c INT
+
+function ctrl_c() {
+    $echo "** Trapped CTRL-C"
+    cleanup
+    exit
+}
+
+declare -a pids
+declare -a keys
+
+function cleanup {
+    if [ ${#pids[@]} -gt 0 ]
+    then
+	$echo "existing pids are ${pids[@]}"
+	for pid in "${pids[@]}"
+	do
+	    $echo "killing pid $pid"
+        kill -9 $pid
+	done
+    else
+	$echo "we do not have any existing processes"
+    fi
+    
+    if [ ${#keys[@]} -gt 0 ]
+    then
+	$echo "existing ring buffer keys are ${keys[@]}"
+       for key in "${keys[@]}"
+       do
+    	   $echo "removing ring buffer $key"
+    	   ipcrm -a
+           #dada_db -k $key -d
+       done
+       $echo "removed all ring buffers"
+    else
+	$echo "we do not have any existing ring buffers"
+    fi
+}
+
+
+WORK_ROOT=/home/hero/code
+project_root=$WORK_ROOT/PAF_pipeline
+hdr_root=$project_root/header/paf_test.header
+dada_gputocpu=$project_root/build/pipeline/dada_gputocpu_test
+dada_cputogpu=$project_root/build/pipeline/dada_cputogpu_test
+
+save_raw_dir=/home/hero/data/data_raw
+save_bmf_dir=/home/hero/data/data_bmf
+
+dada_dtsz=$((1*1024*1024*1024))
+key_input=a000
+key_gpu=b000
+key_output=c000
+block_num=8
+
+# 输入ringbuffer
+dada_db -k $key_input -n $block_num -b $dada_dtsz -p -w&
+pids+=(`echo $! `)
+keys+=(`echo $key_input `)
+sleep 1s 
+# GPU ringbuffer
+dada_db -k $key_gpu -n $block_num -b $dada_dtsz -g 0 -p -w&
+pids+=(`echo $! `)
+keys+=(`echo $key_gpu `)
+sleep 1s 
+
+dada_db -k $key_output -n $block_num -b $dada_dtsz -p -w&
+pids+=(`echo $! `)
+keys+=(`echo $key_output `)
+sleep 1s 
+$echo "created all ring buffers\n"
+
+
+# 清除ringbuffer
+dada_dbnull -k $key_output&
+#dada_dbdisk -k $key_output -D $save_bmf_dir -z -o&
+pids+=(`echo $! `)
+sleep 1s
+#$echo "save output ring buffer\n"
+
+#dada_dbdisk -k $key_input -D $save_raw_dir -o -z -v&
+#pids+=(`echo $! `)
+#sleep 1s
+#$echo "save input ring buffer\n"
+
+# 开启传输
+$dada_gputocpu -g $key_gpu -o $key_output&
+pids+=(`echo $! `)
+sleep 1s 
+$dada_cputogpu -i $key_input -g $key_gpu&
+pids+=(`echo $! `)
+sleep 1s 
+$echo "started transfer\n"
+
+dada_junkdb -g -R 10000 -z -k $key_input -t 20 $hdr_root
+pids+=(`echo $! `)
+sleep 1s 
+$echo "junked CPU ring buffer\n"
+
+cleanup
+
