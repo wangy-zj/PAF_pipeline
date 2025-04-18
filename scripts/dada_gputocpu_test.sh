8787333d90b0709eb21130afafcfc71559a9e50b
wangy-zj <ywang@zhejianglab.com>
2025-04-18 08:08:12 +0000

    "add cpu & gpu test scripts"

--- scripts/dada_gputocpu_test.sh
+++ scripts/dada_gputocpu_test.sh
@@ -42,30 +42,30 @@ function cleanup {
 }
 
 
-WORK_ROOT=/home/hero/code
+WORK_ROOT=/home/wangyu
 project_root=$WORK_ROOT/PAF_pipeline
 hdr_root=$project_root/header/512MHz_beamform_4096B.header
 dada_gputocpu=$project_root/build/pipeline/dada_gputocpu_test
 dada_cputogpu=$project_root/build/pipeline/dada_cputogpu_test
 
-dada_dtsz=$((1000*1024*1024))
+dada_dtsz=$((2000*1024*1024))
 key_input=a000
 key_gpu=b000
 key_output=c000
 block_num=8
 
 # 输入ringbuffer
-dada_db -k $key_input -n $block_num -b $dada_dtsz -p -w&
+dada_db -k $key_input -n $block_num -b $dada_dtsz -p -l -w&
 pids+=(`echo $! `)
 keys+=(`echo $key_input `)
 sleep 1s 
 # GPU ringbuffer
-dada_db -k $key_gpu -n $block_num -b $dada_dtsz -g 0 -p -w&
+dada_db -k $key_gpu -n $block_num -b $dada_dtsz -g 0 -p -l -w&
 pids+=(`echo $! `)
 keys+=(`echo $key_gpu `)
 sleep 1s 
 
-dada_db -k $key_output -n $block_num -b $dada_dtsz -p -w&
+dada_db -k $key_output -n $block_num -b $dada_dtsz -p -l -w&
 pids+=(`echo $! `)
 keys+=(`echo $key_output `)
 sleep 1s 
@@ -87,10 +87,9 @@ pids+=(`echo $! `)
 sleep 1s 
 $echo "started transfer\n"
 
-dada_junkdb -g -R 10000 -z -k $key_input -t 20 $hdr_root
+dada_junkdb -g -R 7000 -z -k $key_input -t 60 $hdr_root
 pids+=(`echo $! `)
 sleep 1s 
 $echo "junked CPU ring buffer\n"
 
 cleanup
-
